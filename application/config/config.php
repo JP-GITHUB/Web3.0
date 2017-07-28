501620a8b48ccd2ff8a16f50834a43b06450b5dd
vlaxjp <xmast3r.feniix9@live.com>
2017-07-28 06:13:15 +0000

    url base

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Web3.0';
+$config['base_url'] = 'https://web3-0-vlaxjp.c9users.io';
 
 /*
 |--------------------------------------------------------------------------
